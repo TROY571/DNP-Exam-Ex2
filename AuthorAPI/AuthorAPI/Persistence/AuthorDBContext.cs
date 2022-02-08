fa7de95b9eb8e09457f66ae77cd7dfaa15f0ac95
Siyu <59789515+TROY571@users.noreply.github.com>
2022-02-08 09:01:28 +0100

    Add files via upload

---
+++ AuthorAPI/AuthorAPI/Persistence/AuthorDBContext.cs
@@ -0,0 +1,16 @@
+﻿using AuthorAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthorAPI.Persistence
+{
+    public class AuthorDBContext:DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = Author.db");
+        }
+    }
+}
